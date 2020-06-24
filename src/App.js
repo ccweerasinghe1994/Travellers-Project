358e01b36cd80ab9c7b3b1c2964a07bdf0f99363
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-06-24 11:09:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import HomePage from "./Pages/HomePage/HomePage.page";
+import Code1 from "./Notes/Universal Selectors/1.note";
 function App() {
+  console.log('Heloo')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomePage/>
+      <Code1/>
     </div>
   );
 }
---
+++ src/Pages/HomePage/HomePage.page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './HomePage.styles.scss'
+
+
+const HomePage = ()=>{
+    return(
+        <header className='header' >Some Text</header>
+    )
+}
+
+export default HomePage;
