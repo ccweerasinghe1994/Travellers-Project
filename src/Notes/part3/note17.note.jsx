e081cc9a0226f6f47d19226747dab6e37e2a92eb
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-06-26 10:28:25 +0530

    adding code snippet component

--- src/Notes/part3/note17.note.jsx
+++ src/Notes/part3/note17.note.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import img_55 from '../../img/55.png';
+import CodeSnippet from "../../util/prismjs/codeSnippets";
+
 
 const Code17 = () => {
     return (
         <div>
             <h1>Changing All Css to SASS </h1>
             <img src={img_55} alt="" className="img"/>
+
+            <h2>Main.scss</h2>
+            <CodeSnippet codeString={main} language='scss'/>
+
         </div>
     )
 }
 
 export default Code17;
+
+const main = `
+@import "abstract/functions";
+@import "abstract/mixins";
+@import "abstract/variables";
+
+@import "base/base";
+@import "base/animations";
+@import "base/typography";
+@import "base/utilities";
+
+@import "pages/home";
+
+@import 'components/button';
+@import 'components/image';
+@import 'components/code.block';
+
+@import 'layout/header';
+`
--- src/util/prismjs/codeSnippets.jsx
+++ src/util/prismjs/codeSnippets.jsx
@@ -1,32 +1,24 @@
 import React from "react";
-import Prism from "prismjs";
-class PrismCode extends React.Component {
-  constructor(props) {
-    super(props);
-    this.ref = React.createRef();
-  }
-  componentDidMount() {
-    this.highlight();
-  }
-  componentDidUpdate() {
-    this.highlight();
-  }
-  highlight = () => {
-    if (this.ref && this.ref.current) {
-      Prism.highlightElement(this.ref.current);
-    }
-  };
-  render() {
-    const { code, plugins, language } = this.props;
+
+ import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+// import SyntaxHighlighter from 'react-syntax-highlighter';
+// import {dracula} from 'react-syntax-highlighter/dist/esm/styles/hljs';
+ import { atomDark,okaidia,duotoneDark,tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
+const CodeSnippet = ({codeString, language}) => {
+
     return (
-      <pre className={!plugins ? "" : plugins.join(" ")}>
-        <code ref={this.ref} className={`language-${language}`}>
-          {code.trim()}
-        </code>
-      </pre>
+        <SyntaxHighlighter customStyle={{
+            border: '1px solid #8a6363',
+            width: '50vw',
+            minWidth:'380px',
+            margin: '10px 0',
+            fontSize: '1.6rem'
+        }}
+                           language={language}
+                           style={tomorrow}>
+            {codeString}
+        </SyntaxHighlighter>
     );
-  }
-}
+};
 
-
-export default PrismCode;;
+export default CodeSnippet;
